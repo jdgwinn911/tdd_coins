80e5540bae5601fc114ac25fc5587eaed5f64938
Josh Gwinn <jdgwinn911@gmail.com>
2018-07-11 13:34:40 -0400

    pass on penny equals 1 cent

--- coins_tdd.rb
+++ coins_tdd.rb
@@ -2,7 +2,16 @@ require "minitest/autorun"
 require_relative "coins.rb"
 
 class TestCoins < Minitest::Test
-    def test_that_hsh_is_a_hash
-        assert_equal(Hash, coin_coin().class)
+    # def test_that_hsh_is_a_hash
+    #     assert_equal(Hash, coin_coin().class)
+    # end
+
+    def test_that_a_penny_is_1_cent
+        assert_equal({:penny => 1}, coin_coin(1))
     end
+
+
+
+
+
 end
